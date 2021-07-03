1972c2b5da73601e0536ec4ef4780943ffa761d1
Vili Tikkanen <39312018+vilitik@users.noreply.github.com>
2021-07-03 18:47:23 +0300

    fix

--- routes/index.js
+++ routes/index.js
@@ -57,16 +57,31 @@ router.post("/newLegacyOrderBookkeeping", function (req, res, next) {
     resp.json().then((data) => {
       res.status(200).json(data);
 
-      fetch(`${mcfInfo.addr}/v1/orders/${data.id}/quick-process`, {
-        method: "POST",
-        headers: {
-          Accept: "*/*",
-          Authorization:
-            "Basic " +
-            Buffer.from(`${mcfInfo.user}:${mcfInfo.password}`).toString(
-              "base64"
-            )
+      fetch(
+        `${mcfInfo.addr}/v0/orders/${data.id}/payments/${data.payments.id}/mark-as-paid`,
+        {
+          method: "POST",
+          headers: {
+            Accept: "*/*",
+            Authorization:
+              "Basic " +
+              Buffer.from(`${mcfInfo.user}:${mcfInfo.password}`).toString(
+                "base64"
+              )
+          }
         }
+      ).then(() => {
+        fetch(`${mcfInfo.addr}/v0/orders/${data.id}/quick-process`, {
+          method: "POST",
+          headers: {
+            Accept: "*/*",
+            Authorization:
+              "Basic " +
+              Buffer.from(`${mcfInfo.user}:${mcfInfo.password}`).toString(
+                "base64"
+              )
+          }
+        });
       });
     });
   });
