1470dcd16c94bc9e514ffa93efd66d39d84f0b86
Vili Tikkanen <39312018+vilitik@users.noreply.github.com>
2021-07-03 14:57:17 +0300

    newLegacyOrderBookkeeping

--- routes/index.js
+++ routes/index.js
@@ -48,7 +48,8 @@ router.post("/newLegacyOrderBookkeeping", function (req, res, next) {
       Authorization:
         "Basic " +
         Buffer.from(`${mcfInfo.user}:${mcfInfo.password}`).toString("base64")
-    }
+    },
+    body: { ...payload }
   }).then((resp) => {
     if (resp.status !== 200) return res.sendStatus(501);
 
