92ff1cb0c03a403ed71c5dd5dcc76ec4d905f973
Vili Tikkanen <39312018+vilitik@users.noreply.github.com>
2021-12-22 20:44:05 +0200

    body saldomuutos

--- routes/sm/index.js
+++ routes/sm/index.js
@@ -4,17 +4,23 @@ var router = express.Router();
 
 router.post("/:product_code",function (req,res,next) {
     const mcfInfo = req.body.mcfInfo;
+    let body = null
     if (!mcfInfo) return res.sendStatus(401);
+    if(req.body.doChanges) {
+        body.quantity = req.body.quantity
+    }
   
     fetch(
     `${mcfInfo.addr}/v1/stock/${req.params.product_code}`,
         {
-        headers: {
-            Accept: "*/*",
-            Authorization:
-            "Basic " +
-            Buffer.from(`${mcfInfo.user}:${mcfInfo.password}`).toString("base64")
-            }
+            method: body ? 'PATCH' : 'GET',
+            headers: {
+                Accept: "*/*",
+                Authorization:
+                "Basic " +
+                Buffer.from(`${mcfInfo.user}:${mcfInfo.password}`).toString("base64")
+            },
+            body
         }
     ).then((resp) => {
       if (resp.status !== 200) return res.sendStatus(501);
