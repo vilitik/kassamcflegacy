0efe4a489cfb154eb9421616b5a418a8ba6ef0c9
Vili Tikkanen <39312018+vilitik@users.noreply.github.com>
2021-12-22 19:32:55 +0200

    s

--- routes/sm/index.js
+++ routes/sm/index.js
@@ -2,24 +2,20 @@ const fetch = require("node-fetch");
 var express = require("express");
 var router = express.Router();
 
-router.post("/:product_id/:variation_id",function (req,res,next) {
+router.post("/:product_code",function (req,res,next) {
     const mcfInfo = req.body.mcfInfo;
     if (!mcfInfo) return res.sendStatus(401);
-
-    let faddr =  `${mcfInfo.addr}/v1/products/${req.params.product_id}`
-    if(req.params.variation_id != 0) faddr += `/variations/${req.params.variation_id}`
-    faddr += '?expand=stock_item'
   
     fetch(
-      faddr,
-      {
+    `${mcfInfo.addr}/v1/stock/${req.params.product_code}`,
+        {
         headers: {
-          Accept: "*/*",
-          Authorization:
+            Accept: "*/*",
+            Authorization:
             "Basic " +
             Buffer.from(`${mcfInfo.user}:${mcfInfo.password}`).toString("base64")
+            }
         }
-      }
     ).then((resp) => {
       if (resp.status !== 200) return res.sendStatus(501);
   
