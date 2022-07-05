c7a7119a0737e2afb8e78a0a262d00976fe8c960
Vili Tikkanen <39312018+vilitik@users.noreply.github.com>
2022-07-05 12:24:39 +0300

    t

--- routes/index.js
+++ routes/index.js
@@ -25,6 +25,7 @@ router.post("/products", function (req, res, next) {
     }
   ).then((resp) => {
     if (!resp) return res.sendStatus(500);
+    console.log(resp)
 
     resp.json().then((data) => {
       console.log(data)
